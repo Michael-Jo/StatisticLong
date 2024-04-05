b35bc2d25859d279600b0946c3ec74319d090914
Michael-Jo <michjo2008@gmail.com>
2024-04-05 11:19:43 +0700

    updated half life 3

--- Lambda.py
+++ Lambda.py
@@ -5,4 +5,12 @@ def kuadrat( num ):
 print( f"Result Kuadrat : { kuadrat(4) }")
 
 #syntax :
-# output = lambda
+# output = lambda variabel_parameter : expression
+ResultKuadrat = lambda abc : abc ** 2
+print( f"Result Kuadrat lambda: { ResultKuadrat(4) }")
+
+def pangkat ( angka, pangkat ):
+    return angka ** pangkat
+print( f"Result pangkat : { pangkat( 4,3 ) }" )
+
+ResultKuadrat = lambda a, b : a ** a
