d7444b7bcf68a2c2dd7ac51e5020979ae10a687f
Michael-Jo <michjo2008@gmail.com>
2024-05-04 17:01:46 +0700

    removed the M

--- Mean.py
+++ Mean.py
@@ -10,15 +10,14 @@ ulang = 0
 def mean(*n):
     hasil = sum(*n) / ulang
     return hasil
-# def median(*n): # extra point's spaghetti code
-#     if (for i in n[i]: 
-#         n[i] = n[-i]):
-#         hasil = n[i] 
-#     else:
-#         hasil = round(n[len.])
-#     return hasil
+def median(*n): # extra point's spaghetti code
+    if (ulang % 2 == 1):
+        hasil = n[round(ulang / 2)]
+    else:
+        hasil = (n[round((ulang / 2) - 0.5)] + n[round((ulang / 2) + 0.5)]) / 2
+    return hasil
 # def modus(*n):
-#     hasil = sum(*n) / ulang
+#     hasil = 
 #     return hasil
 while y.lower() == "y":
     ulang += 1
@@ -28,12 +27,12 @@ while y.lower() == "y":
     if y.lower() == "y":
         os.system( "cls" )
     else:
-        def mean(*n): # tidak bisa digunakan diluar bagian 'else' ini jika fungsinya disini
-            hasil = sum(*n) / ulang
-            return hasil
+        # def mean(*n): # tidak bisa digunakan diluar bagian 'else' ini jika fungsinya disini
+        #     hasil = sum(*n) / ulang
+        #     return hasil
         print(f"Deret Angka : {n}")
         print(f"MEAN : {mean(n)}")
         # Extra point
-        # print(f"MEDIAN : {median(n)}")
+        print(f"MEDIAN : {median(n)}")
         # print(f"MODUS : {modus(n)}")
         break
