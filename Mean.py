2857bad36ecaac90b858d165c5c3c86dfb85b6c8
Michael-Jo <michjo2008@gmail.com>
2024-05-04 09:56:31 +0700

    to be continued extra point

--- Mean.py
+++ Mean.py
@@ -12,7 +12,7 @@ def mean(*n):
     return hasil
 while y.lower() == "y":
     ulang += 1
-    print("-"*15, "NILAI MEAN", "-"*15)
+    print("-"*15, "NILAI MEAN, MEDIAN, MODUS", "-"*15)
     n.append(int(input("Masukan Angka : ")))
     y = input("Lanjut ? (y/t) : ")
     if y.lower() == "y":
@@ -21,5 +21,9 @@ while y.lower() == "y":
         # def mean(*n): # tidak bisa digunakan diluar bagian 'else' ini jika fungsinya disini
         #     hasil = sum(*n) / ulang
         #     return hasil
+        print(f"Deret Angka : {n}")
         print(f"MEAN : {mean(n)}")
+        # Extra point
+        print(f"MEDIAN : {median(n)}")
+        print(f"MODUS : {modus(n)}")
         break
